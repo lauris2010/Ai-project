e0011030018439f5de0fa22794a0276703bcc516
lauris2010 <laurynas.martinkus2009@gmail.com>
2023-09-27 13:33:26 +0300

    fix

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,5 @@
-import LandingHero from "@/components/LandingHero";
-import LandingNavbar from "@/components/LandingNavbar";
+import LandingHero from "../../components/LandingHero";
+import LandingNavbar from "../../components/LandingNavbar";
 import React from "react";
 
 const LandingPage = () => {
